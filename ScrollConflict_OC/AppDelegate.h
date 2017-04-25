7583b2465dad64b54488d28d108139516e640c98
yangyangzhang <zyydeveloper@163.com>
2017-04-25 18:34:17 +0800

    Initial Commit

---
+++ ScrollConflict_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollConflict_OC
+//
+//  Created by Tech-zhangyangyang on 2017/4/25.
+//  Copyright © 2017年 Tech-zhangyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollConflict_OC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScrollConflict_OC
+//
+//  Created by Tech-zhangyangyang on 2017/4/25.
+//  Copyright © 2017年 Tech-zhangyangyang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
